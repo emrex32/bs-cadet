9cd5d670a9c4ea78456395850c46e14ee154dca1
emrex32 <halilemrecadet@gmail.com>
2024-06-26 13:50:26 +0200

    conf changed

--- src/test/java/com/browserstack/SeleniumTest.java
+++ src/test/java/com/browserstack/SeleniumTest.java
@@ -1,7 +1,6 @@
 package com.browserstack;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
@@ -16,22 +15,9 @@ public class SeleniumTest {
     @BeforeMethod(alwaysRun = true)
     @SuppressWarnings("unchecked")
     public void setUp() throws Exception {
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
-        String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
-        String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("os", "Windows");
-        capabilities.setCapability("os_version", "10");
-        capabilities.setCapability("browser", "chrome");
-        capabilities.setCapability("browser_version", "latest");
-        capabilities.setCapability("name", "BStack-[Java] Sample Test"); // test buildName
-        capabilities.setCapability("build", buildName); // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
-        capabilities.setCapability("browserstack.local", browserstackLocal);
-        capabilities.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
-        driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com"), capabilities);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
     }
 
     @AfterMethod(alwaysRun = true)
